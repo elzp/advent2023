9b4fd47c9647153716df2b5ff895d4fb57081957
elzp <piechelzbieta@wp.pl>
2023-12-03 16:31:33 +0100

    Day 2: description of task

--- tasks/2023-11-27/index.ts
+++ tasks/2023-11-27/index.ts
@@ -1,4 +1,8 @@
 // Tutaj skopiuj kod zadania
-export function exampleFunction() {
-    return 4;
-}
+`Święty Mikołaj ma problem. Jego renifery zauważyły, że listy od dzieci z całego świata 
+nie są odpowiednio sortowane. Listy powinny być uporządkowane według priorytetu, który 
+jest wyrażony liczbą - im wyższa liczba, tym wyższy priorytet. Mikołaj potrzebuje systemu, 
+który pomoże mu szybko i efektywnie organizować listy, aby żadne życzenie nie zostało pominięte. 
+Renifery są ekspertami od logistyki, a nie od struktur danych, więc Mikołaj zwraca się o pomoc do 
+rogramistów. Czy jesteś w stanie stworzyć dla niego odpowiednią strukturę danych, która pomoże mu 
+w sortowaniu listów? Czas ucieka!`
