490dce0239e0404da5474808159e256fd98d03c5
elzp <piechelzbieta@wp.pl>
2023-12-03 14:05:51 +0100

    Day 1: 1st test passed

--- tasks/2023-11-26/index.ts
+++ tasks/2023-11-26/index.ts
@@ -1,4 +1,22 @@
 // Tutaj skopiuj kod zadania
-export function exampleFunction() {
-  return 2;
+export class GiftRegistry {
+  
+  childrenIDs: Array<number> = [];
+  presents: Array<Array<string>> = [];
+  
+  addGift(childId: number, present: string) {
+    if(this.childrenIDs.indexOf(childId) < 0) {
+      this.childrenIDs.push(childId);
+      this.presents.push([present]);
+    }
+     else {
+      if(this.presents[childId - 1].indexOf(present) === -1){
+        this.presents[childId - 1].push(present);
+      }
+    }
+  } 
+  removeGift(childId: number, present: string) {}
+  getGiftsForChild(childId: number) {
+    return this.presents[childId - 1];
+  }
 }
