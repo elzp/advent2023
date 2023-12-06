07ce85760ae7c773f1353efc3d05aa7c50036f48
elzp <piechelzbieta@wp.pl>
2023-12-06 20:30:31 +0100

    Day 4: 1st & 2nd test passed

--- tasks/2023-12-05/index.ts
+++ tasks/2023-12-05/index.ts
@@ -7,6 +7,23 @@ wykorzystanie wyników obliczeń. Niestety, nikt z zespołu elfów nie potrafił
 zaimplementować takiej funkcjonalności. Pomóż Mikołajowi i jego pomocnikom w
 rozwiązaniu tego problemu.`
 
-export function memoize() {
+interface fn { (x: any): any };
 
-}
+
+export function memoize(func: fn): any {
+
+    let cache = new Map<string | number, string | number>();
+    return (n: string | number) => {
+      if (cache.has(n)) {
+        console.log('Fetching from cache');
+        const nString = `${n}`
+        return cache.get(n);
+      }
+      else {
+        console.log('Calculating result');
+        let result = func(n);
+        cache.set(n, result);
+        return result;
+      }
+    }
+  }
