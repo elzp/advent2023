09cfb03a5ac9350f8de3bc56fd692b5d68870a1a
elzp <piechelzbieta@wp.pl>
2023-12-16 16:52:59 +0100

    Day 9: 3rd test passed

--- tasks/2023-12-16no2/index.ts
+++ tasks/2023-12-16no2/index.ts
@@ -10,19 +10,31 @@ export interface Tool {
     update: Function,
     dispose: Function,
 };
-
+interface InitObject {
+    initFunctions: Array<Function>,
+    noOfInits: number,
+}
 
 export class Equipment {
-    init: Array<Function> = [];
+    init: InitObject = { initFunctions: [], noOfInits: 0,};
     update: Array<Function> = [];
     dispose: Array<Function> = [];
 
     registerTools(tool: Tool){
-        this.init.push(tool.init);
+        this.init.initFunctions.push(tool.init);
         this.update.push(tool.update);
         this.dispose.push(tool.dispose);
     };
-    initializeTools(){this.init.forEach((it)=>{it();});};
-    updateTools(){ this.update.forEach((it)=>{it();});};
+    initializeTools(){
+        this.init.initFunctions.forEach((it)=>{it();});
+        this.init.noOfInits++;
+    };
+    updateTools(){ 
+        if(this.init.noOfInits === 0){
+            throw Error("Cannot update any tools before initialization.");
+        } else {
+            this.update.forEach((it)=>{it();});
+        }
+    };
     disposeTools(){this.dispose.forEach((it)=>{it();});};
 }
