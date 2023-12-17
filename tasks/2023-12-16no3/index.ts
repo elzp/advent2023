82bdde16545b7c88e3fbd5cfa9cb2199de542776
elzp <piechelzbieta@wp.pl>
2023-12-17 13:55:52 +0100

    Day 10: 1st test passed

--- tasks/2023-12-16no3/index.ts
+++ tasks/2023-12-16no3/index.ts
@@ -5,6 +5,54 @@ do dzieci. Santa postanowił rozwiązać problem wykrywania cykli na swojej drod
 trasę i upewnić się, że każde miasto odwiedzany jest tylko raz. Pomóż Świętemu Mikołajowi w napisaniu
 rozszerzenia do systemu nawigacji, które wykryje cykle w jego ścieżkach i zaproponuje trasę bez zbędnych powtórzeń.`
 
-export function findCyclesBetweenLocations(graph: Record<string, string[]>): string[][] {
-    throw new Error('Function not implemented.');
+function lookForCycle(firstDestination: string, graph: Record<string, string[]>, nextDest: string): string[]{
+	let cycle: string[]  = [];
+	
+	cycle.push(firstDestination);
+	cycle.push(nextDest);
+	let nextDestination = "";
+	let prevDestination = nextDest;
+
+		if (graph[`${prevDestination}`].length === 0) {
+			return cycle;
+		} else {
+		do {
+			nextDestination = graph[`${prevDestination}`][0];
+			prevDestination = nextDestination; cycle.push(nextDestination)
+		} while (firstDestination !== nextDestination)
+		return cycle;
+	}
+}
+
+export function findCyclesBetweenLocations(graph: Record<string, string[]>): Array<Array<string>> | [] {
+
+	let cycles = [];
+
+	for (let step = 0; step < Object.keys(graph).length; step++) {
+		let numberOfKeyInGraph: number = step;
+		let indexOfElInCurrKey: number  = 0;
+		let currentKey: string = Object.keys(graph)[numberOfKeyInGraph]
+
+		let noOfItemsInArrOfCurrKey: number  = graph[`${currentKey}`].length;
+		//check if in arrayn of current key  is any destination
+		if (noOfItemsInArrOfCurrKey === 0) {
+			console.log('no destination available')
+		} else {
+			// check cycles starting from current key
+			while (indexOfElInCurrKey < noOfItemsInArrOfCurrKey){
+				let nextDestination = graph[`${currentKey}`][indexOfElInCurrKey];
+				// check if next destination is in saved cycle
+				let allElInCycles = cycles.length ===  0 ? []: cycles.reduce((prev,current) => {return [...prev, ...current]}, []);
+				if (allElInCycles.indexOf(nextDestination) === -1) {
+					let neCycle = lookForCycle(currentKey, graph, nextDestination)
+					cycles.push(neCycle);
+					indexOfElInCurrKey++;
+				} else {
+					indexOfElInCurrKey = noOfItemsInArrOfCurrKey;
+					continue;
+				}
+			}
+		}
+	}
+	return cycles;
 }
