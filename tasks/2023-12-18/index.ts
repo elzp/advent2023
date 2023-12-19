7cc2e6939a58285ec1c92040354a39b6fc40ac7a
elzp <piechelzbieta@wp.pl>
2023-12-19 19:55:13 +0100

    Day 11: 1st & 2nd tests passed

--- tasks/2023-12-18/index.ts
+++ tasks/2023-12-18/index.ts
@@ -11,6 +11,31 @@ export interface WeightedGraph {
     [key: string]: { [key: string]: number };
   }
   
+  function NextNode(valuesOfPrevNode: { [key: string]: number }, currentPath: string[]) {
+
+	let nodesNames = Object.keys(valuesOfPrevNode);
+	let nodesValues = Object.values(valuesOfPrevNode);
+	let currenLength = nodesNames.length;
+
+	let result = '';
+	let minimum = 0;
+	let indexOfMinEl = 0; 
+	while (currenLength > 0) {
+		minimum = Math.min(...nodesValues);
+		indexOfMinEl = nodesValues.indexOf(minimum);
+
+		if (!currentPath.includes(nodesNames[indexOfMinEl])) {
+				result = nodesNames[indexOfMinEl];
+				break;
+		} else {
+			nodesNames.splice(indexOfMinEl, 1);
+			nodesValues.splice(indexOfMinEl, 1);
+			currenLength--;
+		}
+	}
+	return result !== ''? result : ''
+}
+
 export function findShortestPath(graph: WeightedGraph, startNode: string, endNode: string): string[] | null {
 	const isStartInGraph = Object.keys(graph).includes(startNode) === undefined;
 	const isEndInGraph = Object.values(graph).reduce((prev: string[] | [], current: { [key: string]: number }) => { 
@@ -20,5 +45,20 @@ export function findShortestPath(graph: WeightedGraph, startNode: string, endNod
 	if (!(isStartInGraph || isEndInGraph)) {
 		throw Error("'Invalid or non-existent route'")
 	}
- 	return null;
+ 	
+	
+	let route = [startNode]
+	let nextPath = NextNode(graph[route[0]], [startNode])
+	route.push(nextPath)
+	let step = 1;
+
+	while(nextPath !== '') {
+		nextPath = NextNode(graph[route[step]], route)
+		if(nextPath === ''){break;} else {
+			route.push(nextPath)
+		}
+		step++
+	} 
+
+	return route[route.length - 1] !== endNode ? null : route;
 }
