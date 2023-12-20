06de119beb84b0c35fb753f63d4b79331aa5112c
elzp <piechelzbieta@wp.pl>
2023-12-20 13:03:34 +0100

    Day 12: base of code

---
+++ tasks/2023-12-20/index.ts
@@ -0,0 +1,17 @@
+// Tutaj skopiuj kod zadania
+
+`Jak co roku, w okresie świąt o działalności Świętego Mikołaja szeroko rozpisują się media.
+Dziennikarze chcą wiedzieć jak organizuje on swoją podróż, jakie są jego plany na przyszłość,
+a także co robi w wolnym czasie. Jako, że do świąt zostało już naprawdę niewiele czasu, Mikołaj
+zdecydował się na przeprowadzenie wywiadów o z góry ustalonych ramach czasowych. Niestety, nie
+każdy dziennikarz trzyma się ustalonych zasad. Mikołaj potrzebuje systemu, który pozwoli mu anulować
+przeciągające się rozmowy.`
+
+export async function conductInterviews(
+    subjects: string[],
+    interview: (subject: string) => Promise<string>,
+    timeConstraint: number
+  ): Promise<string[]> {
+    throw new Error('Not implemented');
+  }
+  
